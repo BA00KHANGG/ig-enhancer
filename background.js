5f4ce44115a995254f41060b851d4e35661435bf
Sajjad Ali <sajjadalis@gmail.com>
2024-06-01 08:00:11 +0500

    intial commit

---
+++ background.js
@@ -0,0 +1,62 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({ enabled: true }) // Set default state to enabled
+  updateIconBasedOnDomain()
+})
+
+chrome.runtime.onStartup.addListener(() => {
+  updateIconBasedOnDomain() // Ensure correct icon on browser startup for all tabs
+})
+
+chrome.action.onClicked.addListener(tab => {
+  chrome.storage.local.get({ enabled: true }, data => {
+    const newState = !data.enabled
+    chrome.storage.local.set({ enabled: newState }, () => {
+      sendMessageWithRetry(tab.id, { enabled: newState })
+      updateIconForTab(tab) // Update icon based on the new state and tab URL
+    })
+  })
+})
+
+function sendMessageWithRetry(tabId, message, retryCount = 0) {
+  chrome.tabs.sendMessage(tabId, message, response => {
+    if (chrome.runtime.lastError) {
+      if (retryCount < 3) {
+        setTimeout(
+          () => sendMessageWithRetry(tabId, message, retryCount + 1),
+          1000
+        )
+      } else {
+        console.error(
+          "Failed to send message after retries:",
+          chrome.runtime.lastError.message
+        )
+      }
+      return
+    }
+  })
+}
+
+function updateIconForTab(tab) {
+  chrome.storage.local.get({ enabled: true }, data => {
+    const iconPath = tab.url.includes("Instagram.com")
+      ? data.enabled
+        ? "icon-enabled.png"
+        : "icon-disabled.png"
+      : "icon-disabled.png"
+    chrome.action.setIcon({ path: iconPath, tabId: tab.id })
+  })
+}
+
+function updateIconBasedOnDomain() {
+  chrome.tabs.query({}, tabs => {
+    tabs.forEach(tab => {
+      updateIconForTab(tab)
+    })
+  })
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && tab.url) {
+    updateIconForTab(tab)
+  }
+})
---
+++ contentScript.js
@@ -0,0 +1,34 @@
+function updateCommentsVisibility(hide) {
+  const styleId = "hide-comments-style"
+  let styleElement = document.getElementById(styleId)
+  const urlPath = window.location.pathname
+
+  if (urlPath.includes("/p/") || urlPath.includes("/reel/")) {
+    if (hide) {
+      if (!styleElement) {
+        styleElement = document.createElement("style")
+        styleElement.id = styleId
+        styleElement.innerText = `article[role="presentation"] > div > div:nth-child(2) { 
+          display: none !important; 
+        }`
+        document.head.appendChild(styleElement)
+      }
+    } else {
+      if (styleElement) {
+        styleElement.remove()
+      }
+    }
+  }
+}
+
+chrome.storage.local.get({ enabled: true }, data => {
+  updateCommentsVisibility(data.enabled)
+})
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.enabled !== undefined) {
+    updateCommentsVisibility(message.enabled)
+    sendResponse({ status: "Done" })
+  }
+  return true
+})
